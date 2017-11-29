ef8e4d7fee0e80299ccab7e37cdc7b45a8dd47c8
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2017-11-29 22:29:43 +0100

    add WebSql

--- js/webSql.js
+++ js/webSql.js
@@ -2,6 +2,55 @@
 
 $(document).ready(function() {
 
-    // ya plus qu'a
 
-});
+    /**
+     * Emplacement de méthodes onSuccess, onError et displayResults
+     */
+
+
+
+
+
+
+    if(window.openDatabase){
+        /**
+         * Creation de la base de donnée
+         */
+
+
+    };
+
+
+    /**
+     *  Requête INSERT
+     */
+    $("#persistWebSql").click(function () {
+
+
+    })
+
+
+    /**
+     * Requête SELECT
+     */
+    $("#affichageWebSQL").click(function(){
+
+
+    })
+
+    /**
+     * Requête DELETE
+     */
+    $("#delEntrySQL").click(function(){
+
+
+    })
+
+    /**
+     * Requête DELETE ALL
+     */
+    $("#delBDDSQL").click(function(){
+
+
+    })
+})
